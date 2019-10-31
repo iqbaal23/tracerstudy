7c836ef3b11735379569c19a33d12fd564ce5087
Rexsy Oktiana <rexsyoktiana@gmail.com>
2019-10-31 20:45:49 +0700

    perbaiki card agenda

--- frontend/views/site/agenda.php
+++ frontend/views/site/agenda.php
@@ -12,19 +12,21 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="uk-light uk-grid-medium uk-child-width-1-3@m uk-grid-small uk-grid-match tm-margin-large-top uk-text-center" data-uk-grid>
         <?php foreach ($agenda as $data) : ?>
-            <div data-uk-scrollspy="cls: uk-animation-slide-bottom; repeat: true; delay: 100">
-                <a href="#" class="uk-card uk-card-box uk-card-body uk-border-rounded">
-                    <h3><?= $data['judul'] ?></h3><hr>
-                    <!-- <span class="uk-border-rounded">
+        <div data-uk-scrollspy="cls: uk-animation-slide-bottom; repeat: true; delay: 100">
+            <a href="#" class="uk-card uk-card-box uk-card-body uk-border-rounded">
+                <h3><?= $data['judul'] ?></h3>
+                <hr>
+                <!-- <span class="uk-border-rounded">
                         <img src="<?php //Yii::$app->params['front']
-                                        ?><?php //$data->file 
-                                ?>" alt="" />
+                                    ?><?php //$data->file 
+                                            ?>" alt="" />
                     </span> -->
-                    <p><?= $data['isi'] ?></p><hr>
-                    <small>Created By: <?= $data['username']?></small>
-                </a>
-            </div>
+                <p><?= $data['isi'] ?></p>
+                <hr>
+                <small>Created By: <?= $data['username']?></small>
+            </a>
+        </div>
+        <?php endforeach 
+        ?>
     </div>
-<?php endforeach ?>
-</div>
 </div>
